6c049b2b73a1d00ff33976191a88533ff115edbf
Clouds Flowing <clouds.flowing@gmail.com>
2024-03-10 17:32:59 +0800

    switch to esp-idf-template

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,14 @@
 #![no_std]
 #![no_main]
 
-extern crate alloc;
-use core::mem::MaybeUninit;
-use esp_hal::{clock::ClockControl, peripherals::Peripherals, prelude::*, Delay};
-use esp_backtrace as _;
-use esp_println::println;
+#[no_mangle]
+fn main() {
+  // It is necessary to call this function once. Otherwise some patches to the runtime
+  // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
+  esp_idf_svc::sys::link_patches();
 
-// use esp_wifi::{initialize, EspWifiInitFor};
-// use esp_hal::{timer::TimerGroup, Rng};
+  // Bind the log crate to the ESP Logging facilities
+  esp_idf_svc::log::EspLogger::initialize_default();
 
-#[global_allocator]
-static ALLOCATOR: esp_alloc::EspHeap = esp_alloc::EspHeap::empty();
-
-fn init_heap() {
-  const HEAP_SIZE: usize = 32 * 1024;
-  static mut HEAP: MaybeUninit<[u8; HEAP_SIZE]> = MaybeUninit::uninit();
-
-  unsafe {
-    ALLOCATOR.init(HEAP.as_mut_ptr() as *mut u8, HEAP_SIZE);
-  }
-}
-#[entry]
-fn main() -> ! {
-  init_heap();
-  let peripherals = Peripherals::take();
-  let system = peripherals.SYSTEM.split();
-
-  let clocks = ClockControl::max(system.clock_control).freeze();
-  let mut delay = Delay::new(&clocks);
-
-  // setup logger
-  // To change the log_level change the env section in .cargo/config.toml
-  // or remove it and set ESP_LOGLEVEL manually before running cargo run
-  // this requires a clean rebuild because of https://github.com/rust-lang/cargo/issues/10358
-  esp_println::logger::init_logger_from_env();
-  log::info!("Logger is setup");
-  println!("Hello world!");
-  // let timer = TimerGroup::new(peripherals.TIMG1, &clocks).timer0;
-  // let _init = initialize(
-  //   EspWifiInitFor::Wifi,
-  //   timer,
-  //   Rng::new(peripherals.RNG),
-  //   system.radio_clock_control,
-  //   &clocks,
-  // )
-  // .unwrap();
-  loop {
-    println!("Loop...");
-    delay.delay_ms(500u32);
-  }
+  log::info!("Hello, world!");
 }
