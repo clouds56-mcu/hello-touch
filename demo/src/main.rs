ec2db53a0b74ee08933cae7e47b25112f959801f
Clouds Flowing <clouds.flowing@gmail.com>
2024-03-12 02:09:44 +0800

    use embedded_graphics

--- demo/src/main.rs
+++ demo/src/main.rs
@@ -4,8 +4,9 @@
 #[macro_use] extern crate log;
 extern crate alloc;
 
-use alloc::vec::Vec;
-use esp_idf_svc::{hal::delay::Delay, sys::{esp_err_to_name, esp_lcd_new_rgb_panel, esp_lcd_panel_draw_bitmap, esp_lcd_panel_init, esp_lcd_panel_reset}};
+use esp_idf_svc::{hal::delay::Delay, sys::{esp_err_to_name, esp_lcd_new_rgb_panel, esp_lcd_panel_draw_bitmap, esp_lcd_panel_init, esp_lcd_panel_reset, esp_lcd_rgb_panel_get_frame_buffer}};
+use embedded_graphics::{geometry::{Point, Size}, pixelcolor::{Rgb565, RgbColor}, primitives::{Primitive, PrimitiveStyle, Rectangle}, Drawable};
+use embedded_graphics_framebuf::{backends::{EndianCorrectedBuffer, EndianCorrection}, FrameBuf};
 
 macro_rules! pin {
   (none) => { -1 };
@@ -26,25 +27,49 @@ fn main() {
 
   let delay = Delay::new_default();
   let lcd_config = consts::lcd_config();
-  let mut panel = core::ptr::null_mut();
-  unsafe {
+  let panel = unsafe {
+    let mut panel = core::ptr::null_mut();
     esp_lcd_new_rgb_panel(&lcd_config as *const _, &mut panel as *mut _);
     esp_lcd_panel_reset(panel);
     let err = esp_lcd_panel_init(panel);
     if err != 0 { error!("{:?}", core::ffi::CStr::from_ptr(esp_err_to_name(err))); loop {} }
+    panel
   };
 
+  let buffer_ptr = unsafe {
+    let mut ptr = core::ptr::null_mut();
+    let err = esp_lcd_rgb_panel_get_frame_buffer(panel, 1, &mut ptr as *mut _);
+    if err != 0 { error!("{:?}", core::ffi::CStr::from_ptr(esp_err_to_name(err))); loop {} }
+    ptr as *mut Rgb565
+  };
+  let buffer = unsafe { core::slice::from_raw_parts_mut(buffer_ptr, 800*480) };
+  let mut frame = FrameBuf::new(EndianCorrectedBuffer::new(buffer, EndianCorrection::ToLittleEndian), 800, 480);
+
   info!("Hello, world!");
+  delay.delay_ms(1000);
+
+  const COLORS: [Rgb565; 8] = [Rgb565::BLUE, Rgb565::CYAN, Rgb565::GREEN, Rgb565::BLACK, Rgb565::RED, Rgb565::WHITE, Rgb565::YELLOW, Rgb565::MAGENTA];
   for i in 0..48 {
-    let color_data = (0..8000).map(|_| (i as u16 / 2).wrapping_mul(38259)).collect::<Vec<_>>();
-    info!("{} {}", color_data.len(), color_data[color_data.len() -1]);
-    unsafe {
-      esp_lcd_panel_draw_bitmap(panel, 0, i*10, 800, (i+1)*10, color_data.as_ptr() as *const _);
-    }
+    let color = if i <= 10 || i >= 40 {
+      COLORS[i as usize % COLORS.len()]
+    } else {
+      COLORS[0]
+    };
+    info!("{} {:?}", i, color);
+    Rectangle::new(Point::new(0, i*10), Size::new(800, 10))
+      .into_styled(PrimitiveStyle::with_fill(color.into()))
+      .draw(&mut frame)
+      .unwrap();
+  }
+  unsafe {
+    esp_lcd_panel_draw_bitmap(panel, 0, 0, 800, 480, buffer_ptr as *const _);
   }
 
   loop {
     info!("loop...");
+    // unsafe {
+    //   esp_lcd_rgb_panel_restart(panel);
+    // }
     delay.delay_ms(1000);
   }
 }
